f964bcfa13be2560c74155d9ff8e73d68d738d66
zbjoy <664266432@qq.com>
2024-10-05 21:01:36 +0800

    在c语言中使用内联汇编实现字符显示函数(show_msg)

--- m_os_x86_learning/os/start/source/loader/loader_16.c
+++ m_os_x86_learning/os/start/source/loader/loader_16.c
@@ -1,6 +1,21 @@
 __asm__(".code16gcc");
 
+static void show_msg(const char* msg)
+{
+    char c;
+
+    while ((c = *msg++) != '\0')
+    {
+        __asm__ __volatile__(
+            "mov $0xe, %%ah\n\t"
+            "mov %[ch], %%al\n\t"
+            "INT $0x10" : : [ch]"r"(c)
+        );
+    }
+}
+
 void loader_entry(void) 
 {
-    for (;;); // 死循环
+    show_msg("loading......\n\r");
+    for (;;) {}; // 死循环
 }
