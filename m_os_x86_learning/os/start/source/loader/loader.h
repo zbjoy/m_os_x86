778dd1cbf4a8c11eb33602d56fecd832503a6784
zbjoy <664266432@qq.com>
2024-10-10 18:05:59 +0800

    添加内存检测函数在laoder_16中(detect_memory)

--- m_os_x86_learning/os/start/source/loader/loader.h
+++ m_os_x86_learning/os/start/source/loader/loader.h
@@ -1,4 +1,16 @@
 #ifndef LOADER_H
 #define LOADER_H
 
+#include "comm/boot_info.h"
+#include "comm/types.h"
+
+typedef struct SMAP_entry {
+    uint32_t BaseL;
+    uint32_t BaseH;
+    uint32_t LenghtL;
+    uint32_t LenghtH;
+    uint32_t Type;  // entry Type, 值为1时 表明 为我们可用的RAM空间
+    uint32_t ACPI; // extended, bit0 = 1 时 表明此条目应当被忽略
+} __attribute__((packed)) SMAP_entry_t;
+
 #endif /* LOADER_H */
--- m_os_x86_learning/os/start/source/loader/loader_16.c
+++ m_os_x86_learning/os/start/source/loader/loader_16.c
@@ -1,5 +1,9 @@
 __asm__(".code16gcc");
 
+#include "loader.h"
+
+static boot_info_t boot_info;
+
 static void show_msg(const char* msg)
 {
     char c;
@@ -14,8 +18,49 @@ static void show_msg(const char* msg)
     }
 }
 
+static void detect_memory(void)
+{
+    SMAP_entry_t  smap_entry;
+    uint32_t contID = 0, signature, bytes;
+    show_msg("try to detect memory...\r\n");
+
+    boot_info.ram_region_count = 0;
+
+    for (int i = 0; i < BOOT_RAM_REGION_MAX; i++)
+    {
+        SMAP_entry_t* entry = &smap_entry;
+        __asm__ __volatile__ ("INT $0x15"
+                            : "=a"(signature), "=c"(bytes), "=b"(contID)
+                            : "a"(0xE820), "b"(contID), "c"(24), "d"(0x534D4150), "D"(entry));
+
+        if (signature != 0x534D4150) 
+        {
+            show_msg("failed\r\n");
+			return; // error
+        }
+		if (bytes > 20 && (entry->ACPI & 0x0001) == 0)
+        {
+			// ignore this entry
+            continue;
+		}
+
+        if (entry->Type == 1)
+        {
+            boot_info.ram_region_cfg[boot_info.ram_region_count].start = entry->BaseL;
+            boot_info.ram_region_cfg[boot_info.ram_region_count].size = entry->LenghtL;
+            boot_info.ram_region_count++;
+        }
+        if (contID == 0)
+        {
+            break;
+        }
+    }
+    show_msg("ok...\r\n");
+}
+
 void loader_entry(void) 
 {
     show_msg("loading......\n\r");
+    detect_memory();
     for (;;) {}; // 死循环
 }
