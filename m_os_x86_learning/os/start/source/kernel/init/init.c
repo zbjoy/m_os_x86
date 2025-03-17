8b764f872a234a1404d458edea02de104266a47f
zbjoy <664266432@qq.com>
2025-03-17 22:16:08 +0800

    add test int init.c and success

--- m_os_x86_learning/os/start/source/kernel/init/init.c
+++ m_os_x86_learning/os/start/source/kernel/init/init.c
@@ -54,6 +54,27 @@ void list_test() {
         list_insert_last(&list, node);
     }
     log_printf("list_test: first=0x%x, last=0x%x, list_is_empty=%d, list_count=%d", list_first(&list), list_last(&list), list_is_empty(&list), list_count(&list));
+
+    for (int i = 0; i < 5; i++)  {
+        list_node_t* node = list_remove_first(&list);
+        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
+    }
+    log_printf("list_test: first=0x%x, last=0x%x, list_is_empty=%d, list_count=%d", list_first(&list), list_last(&list), list_is_empty(&list), list_count(&list));
+
+    list_init(&list);
+    for (int i = 0; i < 5; i++) {
+        list_node_t* node = nodes + i;
+        log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_last(&list, node);
+    }
+    log_printf("list_test: first=0x%x, last=0x%x, list_is_empty=%d, list_count=%d", list_first(&list), list_last(&list), list_is_empty(&list), list_count(&list));
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t* node = nodes + i;
+        log_printf("remove last from list: %d, 0x%x", i, (uint32_t)node);
+        list_remove(&list, node);
+    }
+    log_printf("list_test: first=0x%x, last=0x%x, list_is_empty=%d, list_count=%d", list_first(&list), list_last(&list), list_is_empty(&list), list_count(&list));
 }
 
 void init_main(void)
