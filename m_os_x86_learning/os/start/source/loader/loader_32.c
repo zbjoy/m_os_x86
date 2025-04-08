2d8f68a8ffe4ff638e0882c0c1e71f28e6e08c2a
zbjoy <664266432@qq.com>
2025-04-08 22:17:53 +0800

    add function enable_page_mode 并且打开分页机制使用4MB大小的大页模式

--- m_os_x86_learning/os/start/source/loader/loader_32.c
+++ m_os_x86_learning/os/start/source/loader/loader_32.c
@@ -70,6 +70,27 @@ static void die(int code)
     for (;;) {}
 }
 
+#define PDE_P (1 << 0) // 页目录项有效
+#define PDE_RW (1 << 1) // 页目录项可读写 (loader处还有数据在里面)
+#define PDE_PS (1 << 7) // 页目录项指向一个页表 (0: 指向页目录, 1: 指向页表)
+#define CR4_PSE (1 << 4) // 允许大页模式 (以 4MB为单位的页)
+#define CR0_PG (1 << 31) // 开启分页机制
+void enable_page_mode(void) {
+    // 硬件要求对齐到 4KB 的地址边界处
+    static uint32_t page_dir[1024] __attribute__((aligned(4096))) = {
+        [0] = PDE_P | PDE_RW | PDE_PS | 0
+    };
+
+    uint32_t cr4 = read_cr4();
+    write_cr4(cr4 | CR4_PSE);
+
+    write_cr3((uint32_t)page_dir);
+
+    // 开启分页机制 cr0 的 pg(page_enable) 位必须为 1
+    write_cr0(read_cr0() | CR0_PG);
+
+}
+
 void load_kernel(void)
 {
     read_disk(100, 500, (uint8_t*)SYS_KERNEL_LOAD_ADDR);
@@ -79,6 +100,8 @@ void load_kernel(void)
     {
         die(-1);
     }
+
+    enable_page_mode();
 
     ((void (*)(boot_info_t*))kernel_entry)(&boot_info);
     for (;;)
