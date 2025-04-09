164ba9bf4c2eba20fa02073b9f9fefdd5ed4b49c
zbjoy <664266432@qq.com>
2025-04-09 21:57:43 +0800

    add union pde_t(page directory entry), union pte_t(page table entry)

--- m_os_x86_learning/os/start/source/kernel/include/cpu/mmu.h
+++ m_os_x86_learning/os/start/source/kernel/include/cpu/mmu.h
@@ -3,4 +3,37 @@
 
 #include "comm/types.h"
 
+typedef union _pde_t { // page directory entry (第一个表, 页目录表)
+    uint32_t v;
+    struct {
+        uint32_t present : 1; // 是否存在
+        uint32_t write_enable : 1; // 是否可写
+        uint32_t user_mode_acc : 1; // 是否用户态
+        uint32_t write_through : 1; // 是否写透
+        uint32_t cache_disable : 1; // 是否缓存
+        uint32_t accessed : 1; // 是否被访问过
+        uint32_t  : 1; // 没有使用到, 用匿名的
+        uint32_t ps : 1; // 是否为大页
+        uint32_t  : 4; // 没有使用到, 用匿名的
+        uint32_t phy_pt_addr : 20; // 物理页表地址
+    };
+} pde_t;
+
+typedef union _pte_t { // page table entry (第一个表, 页目录表)
+    uint32_t v;
+    struct {
+        uint32_t present : 1; // 是否存在
+        uint32_t write_enable : 1; // 是否可写
+        uint32_t user_mode_acc : 1; // 是否用户态
+        uint32_t write_through : 1; // 是否写透
+        uint32_t cache_disable : 1; // 是否缓存
+        uint32_t accessed : 1; // 是否被访问过
+        uint32_t dirty : 1; // 是否被修改过
+        uint32_t pat : 1; // 是否支持PAT
+        uint32_t global : 1; // 是否全局
+        uint32_t  : 3; // 没有使用到, 用匿名的
+        uint32_t phy_page_addr : 20; // 物理页地址
+    };
+} pte_t;
+
 #endif
