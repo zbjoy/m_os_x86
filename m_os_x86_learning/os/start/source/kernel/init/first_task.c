e154d629982dfaf6f948b5c56ffdb0bb0f0c6a12
zbjoy <664266432@qq.com>
2025-04-13 21:13:58 +0800

    添加了文件first_task.c与first_task_entry.S

---
+++ m_os_x86_learning/os/start/source/kernel/init/first_task.c
@@ -0,0 +1,11 @@
+#include "kernel/include/core/task.h"
+#include "kernel/include/tools/log.h"
+
+int first_task_main(void) {
+    for (;;) {
+        log_printf("first task.\n");
+        sys_msleep(1000);
+    }
+
+    return 0;
+}
