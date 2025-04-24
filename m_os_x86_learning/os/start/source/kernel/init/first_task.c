8ecb5429ab383d627322bd07db1a83f91a9bc9c2
zbjoy <664266432@qq.com>
2025-04-24 22:52:03 +0800

    现在first_task运行在特权级3下,不能再访问内核的代码了,所以log_printf与sys_sleep不能再调用了,之后会加入系统调用来实现哪些功能

--- m_os_x86_learning/os/start/source/kernel/init/first_task.c
+++ m_os_x86_learning/os/start/source/kernel/init/first_task.c
@@ -3,8 +3,8 @@
 
 int first_task_main(void) {
     for (;;) {
-        log_printf("first task.\n");
-        sys_sleep(1000);
+        // log_printf("first task.\n");
+        // sys_sleep(1000);
     }
 
     return 0;
