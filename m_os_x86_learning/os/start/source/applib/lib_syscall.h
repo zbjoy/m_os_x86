1a2ccf7014d4e6adb89c92a520c09533f153ad35
zbjoy <664266432@qq.com>
2025-04-29 18:32:57 +0800

    create function sys_call

--- m_os_x86_learning/os/start/source/applib/lib_syscall.h
+++ m_os_x86_learning/os/start/source/applib/lib_syscall.h
@@ -1,12 +1,30 @@
 #ifndef LIB_SYSCALL_H
 #define LIB_SYSCALL_H
 
+typedef struct _syscall_args_t {
+    int id;
+    int arg0;
+    int arg1;
+    int arg2;
+    int arg3;
+} syscall_args_t;
+
+#define SYS_sleep 0
+
+static inline int sys_call(syscall_args_t *args) {
+    
+}
+
 static inline void ms_sleep(int ms) {
     if (ms <= 0) {
         return;
     }
 
     // 通过调用门实现系统调用
+    syscall_args_t args;
+    args.id = SYS_sleep;
+    args.arg0 = ms;
+    sys_call(&args); // 参数的数量
 }
 
 
