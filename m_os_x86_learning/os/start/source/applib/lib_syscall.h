3a009a81aed2626c42d2a58a07806badadb6240f
zbjoy <664266432@qq.com>
2025-05-02 15:42:38 +0800

    完成sys_call的参数压栈(由arg3到arg0,id)

--- m_os_x86_learning/os/start/source/applib/lib_syscall.h
+++ m_os_x86_learning/os/start/source/applib/lib_syscall.h
@@ -20,7 +20,13 @@ static inline int sys_call(syscall_args_t *args) {
     uint32_t addr[] = {0, SELECTOR_SYSCALL | 0};
 
     __asm__ __volatile__(
-        "lcalll *(%[a])"::[a]"r"(addr) // 这里的 addr 是一个指针，指向一个数组 
+        "push %[arg3]\n\t"
+        "push %[arg2]\n\t"
+        "push %[arg1]\n\t"
+        "push %[arg0]\n\t"
+        "push %[id]\n\t"
+        "lcalll *(%[a])"::
+        [arg3]"r"(args->arg3), [arg2]"r"(args->arg2), [arg1]"r"(args->arg1), [arg0]"r"(args->arg0), [id]"r"(args->id), [a]"r"(addr) // 这里的 addr 是一个指针，指向一个数组 
     );
 }
 
