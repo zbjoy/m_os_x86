3f1a740262e3e93bbaf7e3c60590c8999dd2d60e
zbjoy <664266432@qq.com>
2025-05-08 00:19:24 +0800

    add file: fs, include/fs, 实现文件相关函数声明

---
+++ m_os_x86_learning/os/start/source/kernel/fs/fs.c
@@ -0,0 +1,21 @@
+#include "kernel/include/fs/fs.h"
+
+int sys_open(const char* name, int flags, ...) {
+
+}
+
+int sys_read(int file, char* ptr, int len) {
+
+}
+
+int sys_write(int file, char* ptr, int len) {
+
+}
+
+int sys_lseek(int file, int ptr, int dir) {
+
+}
+
+int sys_close(int file) {
+
+}
