0876eb07fc951b67886095d575d04f34c6fd92ba
zbjoy <664266432@qq.com>
2025-05-09 21:47:20 +0800

    add test code int file shell/main.c

--- m_os_x86_learning/os/start/source/shell/main.c
+++ m_os_x86_learning/os/start/source/shell/main.c
@@ -1,6 +1,9 @@
 #include "lib_syscall.h"
 
 int main(int argc, char** argv) {
+    for (int i = 0; i < argc; i++) {
+        print_msg("arg = %s\n", (int)argv[i]);
+    }
     for (;;) {
         ms_sleep(1000);
     }
