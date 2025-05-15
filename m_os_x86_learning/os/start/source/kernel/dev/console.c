0f318f2fc57ba7c7c29544d40b187cb52810261c
zbjoy <664266432@qq.com>
2025-05-15 22:47:48 +0800

    add function read_cursor, update_cursor_pos

--- m_os_x86_learning/os/start/source/kernel/dev/console.c
+++ m_os_x86_learning/os/start/source/kernel/dev/console.c
@@ -1,9 +1,39 @@
 #include "kernel/include/dev/console.h"
 #include "kernel/include/tools/klib.h"
+#include "comm/cpu_instr.h"
 
 #define CONSOLE_NR 1 // 目前只做一个屏幕的处理
 
 static console_t console_buf[CONSOLE_NR];
+
+static void read_cursor_pos(void) {
+    int pos;
+
+    // 获得光标位置
+    outb(0x3D4, 0xF); // 读取光标位置的低字节
+    pos = inb(0x3D5); // 读取光标位置的低字节
+
+    outb(0x3D5, 0xE); // 读取光标位置的高字节
+    pos |= inb(0x3D5) << 8; // 读取光标位置的高字节
+
+    return pos;
+}
+
+static void update_cursor_pos(console_t* console) {
+    // 当前光标位置
+    uint16_t pos = console->cursor_row * console->display_cols + console->cursor_col;
+
+    // 设置光标位置 (将光标位置写入屏幕)
+    // 写低 8 位
+    outb(0x3D4, 0xF); // 读取光标位置的低字节
+    outb(0x3D5, (uint8_t)(pos & 0xFF)); // 写入光标位置的低字节
+
+    // 写高 8 位
+    outb(0x3D5, 0xE); // 读取光标位置的高字节
+    outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF)); // 写入光标位置的高字节
+
+    return pos;
+}
 
 static void erase_rows(console_t* console, int start, int end) {
     disp_char_t* disp_start = console->disp_base + console->display_cols * start;
@@ -76,9 +106,12 @@ int console_init(void) {
     for (int i = 0; i < CONSOLE_NR; i++) {
         console_t* console = console_buf + i;
 
+        // 读取当前光标位置
+        int cursor_pos = read_cursor_pos();
+
         // 光标位置初始化
-        console->cursor_row = 0;
-        console->cursor_col = 0;
+        console->cursor_row = cursor_pos / console->display_cols;
+        console->cursor_col = cursor_pos % console->display_cols;
 
 
         console->display_cols = CONSOLE_COL_MAX;
@@ -89,7 +122,7 @@ int console_init(void) {
 
         console->disp_base = (disp_char_t*)(CONSOLE_DISP_ADDR + i * (CONSOLE_COL_MAX * CONSOLE_ROW_MAX)); // disp_char_t 是一个 2 字节的结构体, 每次给它的指针 +1 相当于加 2 个字节, 这里是一个 80 * 25 的显示缓冲区
 
-        clear_display(console);
+        // clear_display(console);
     }
     return 0;
 }
@@ -115,6 +148,8 @@ int console_write(int console, char* data, int size) {
         // show_char(c, ch);
 
     }
+    
+    update_cursor_pos(c); // 更新光标位置
     return len;
 }
  // console: 写的是哪个控制台, data: 要写入的数据, size: 要写入的数据的长度
