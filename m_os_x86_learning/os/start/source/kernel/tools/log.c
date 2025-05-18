a413c2cf61531ba4088f4aa3d89ff4979fda4870
zbjoy <664266432@qq.com>
2025-05-18 16:06:27 +0800

    modify log_printf(提供控制台输出代替串口输出)

--- m_os_x86_learning/os/start/source/kernel/tools/log.c
+++ m_os_x86_learning/os/start/source/kernel/tools/log.c
@@ -4,13 +4,17 @@
 #include "kernel/include/tools/klib.h"
 #include "kernel/include/cpu/irq.h"
 #include "kernel/include/ipc/mutex.h"
+#include "kernel/include/dev/console.h"
 
 static mutex_t mutex;
 
+#define LOG_USE_COM 0
 #define COM1_PORT 0x3F8
 
 void log_init(void) {
     mutex_init(&mutex);
+
+#if LOG_USE_COM
     outb(COM1_PORT + 1, 0x00); // 关掉串行接口内部的中断
     outb(COM1_PORT + 3, 0x80); // 设置 DLAB 为 1(与速度有关)
     outb(COM1_PORT + 0, 0x03); // 设置波特率为 38400
@@ -18,6 +22,7 @@ void log_init(void) {
     outb(COM1_PORT + 3, 0x03); // 开启 FIFO，设置 8 位数据位，1 位停止位
     outb(COM1_PORT + 2, 0xC7); // 设置输出格式为 8 位数据位，无校验位，1 位停止位
     outb(COM1_PORT + 4, 0x0F); // 开启 IR 接收器，允许发送数据
+#endif
 
 }
 
@@ -35,6 +40,7 @@ void log_printf(const char *fmt,...) {
     // irq_state_t state = irq_enter_protection();
     mutex_lock(&mutex);
 
+#if LOG_USE_COM
     const char* p = str_buf;
     while (*p != '\0') {
         while ((inb(COM1_PORT + 5) & (1 << 6)) == 0); // 等待空闲 (检查串行接口是否在忙)
@@ -42,6 +48,11 @@ void log_printf(const char *fmt,...) {
     }
     outb(COM1_PORT, '\r'); // 输出回车符
     outb(COM1_PORT, '\n'); // 输出换行符
+#else 
+    console_write(0, str_buf, kernel_strlen(str_buf)); // 直接输出到控制台
+    char ch = '\n';
+    console_write(0, &ch, 1); // 输出换行符
+#endif
 
     // 退出临界区, 恢复中断
     // irq_leave_protection(state);
