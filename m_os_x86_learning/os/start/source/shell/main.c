d00dea914e9771c0ea3ca8c5c42f7815037ef57e
zbjoy <664266432@qq.com>
2025-05-19 16:53:11 +0800

    添加测试用例

--- m_os_x86_learning/os/start/source/shell/main.c
+++ m_os_x86_learning/os/start/source/shell/main.c
@@ -11,8 +11,18 @@ int main(int argc, char** argv) {
     printf("abef\b\b\b\bcd\n"); // cdef
     printf("abcd\x7f;fg\n"); // abc;fg
     
-    printf("\0337Hello,World!\0338123\n"); // ESC 7,8 输出123lo,word!
-    printf("\033[31;42mHello, World!!\033[39;49m123\n"); // ESC [pn m, Hello,world红色，其余绿色
+    // printf("\0337Hello,World!\0338123\n"); // ESC 7,8 输出123lo,word!
+    // printf("\033[31;42mHello, World!!\033[39;49m123\n"); // ESC [pn m, Hello,world红色，其余绿色
+
+    printf("123\033[2DHello,word!\n");                // 光标左移2，1Hello,word!
+    printf("123\033[2CHello,word!\n");                // 光标右移2，123  Hello,word!
+
+    printf("\033[31m");            // ESC [pn m, Hello,world红色，其余绿色
+    printf("\033[10;10H test!\n"); // 定位到10, 10，test!
+    printf("\033[20;20H test!\n"); // 定位到20, 20，test!
+    printf("\033[32;25;39m123\n"); // ESC [pn m, Hello,world红色，其余绿色
+
+    printf("\033[2J"); // 清屏
 
     printf("Hello World!!\n");
     printf("Hello From Shell!!\n");
