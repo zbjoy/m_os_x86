377f2f89716871474705bb65e42c8abd59e90742
zbjoy <664266432@qq.com>
2025-05-19 22:03:36 +0800

    add function: do_handler_kbd

--- m_os_x86_learning/os/start/source/kernel/dev/kbd.c
+++ m_os_x86_learning/os/start/source/kernel/dev/kbd.c
@@ -1,5 +1,7 @@
 #include "kernel/include/dev/kbd.h"
 #include "kernel/include/cpu/irq.h"
+#include "kernel/include/tools/log.h"
+#include "comm/cpu_instr.h"
 
 void kbd_init(void) {
     irq_install(IRQ1_KEYBOARD, (irq_handler_t)exception_handler_kbd);
@@ -8,5 +10,14 @@ void kbd_init(void) {
 
 
 void do_handler_kbd(exception_frame_t *frame) { // 通过汇编实现
+    uint32_t status = inb(KBD_PORT_STAT); // 读取键盘状态端口
+    if (!(status & KBD_STAT_RECV_READY)) { // 是其他事情(不是键盘导致的中断)
+        pic_send_eoi(IRQ1_KEYBOARD); // 发送结束信号
+        return;
+    }
 
+    uint8_t raw_code = inb(KBD_PORT_DATA); // 读取键盘数据端口
+    log_printf("key code: %x\n", raw_code); // 打印键盘扫描码
+
+    pic_send_eoi(IRQ1_KEYBOARD); // 发送结束信号
 }
--- m_os_x86_learning/os/start/source/kernel/include/dev/kbd.h
+++ m_os_x86_learning/os/start/source/kernel/include/dev/kbd.h
@@ -2,9 +2,11 @@
 #ifndef KDB_H
 #define KDB_H
 
-#define KDB_PORT_DATA 0x60  // 键盘数据端口
-#define KDB_PORT_STAT  0x64  // 键盘命令端口 (有两个功能: 读和写), 读: 读键盘控制器的状态; 写: 写命令到键盘控制器(往0x64端口写命令), 这里是读命令
+#define KBD_PORT_DATA 0x60  // 键盘数据端口
+#define KBD_PORT_STAT  0x64  // 键盘命令端口 (有两个功能: 读和写), 读: 读键盘控制器的状态; 写: 写命令到键盘控制器(往0x64端口写命令), 这里是读命令
 #define KBD_PORT_CMD   0x64  // 键盘命令端口 (有两个功能: 读和写), 读: 读键盘控制器的状态; 写: 写命令到键盘控制器(往0x64端口写命令), 这里是写命令
+
+#define KBD_STAT_RECV_READY (1 << 0) // 键盘接收就绪标志位 (键盘控制器的状态寄存器的第 0 位)
 
 
 void kbd_init(void); // 初始化键盘
