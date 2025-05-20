3b479a7626a99ce792139446a089c80d38336e2b
zbjoy <664266432@qq.com>
2025-05-20 21:53:36 +0800

    添加键值映射表

--- m_os_x86_learning/os/start/source/kernel/dev/kbd.c
+++ m_os_x86_learning/os/start/source/kernel/dev/kbd.c
@@ -3,9 +3,86 @@
 #include "kernel/include/tools/log.h"
 #include "comm/cpu_instr.h"
 
+// 通过键值映射实现对应键值的获取
+static const key_map_t map_table[] = {
+    [0x2] = {'1', '!'},
+    [0x3] = {'2', '@'},
+    [0x4] = {'3', '#'},
+    [0x5] = {'4', '$'},
+    [0x6] = {'5', '%'},
+    [0x7] = {'6', '^'},
+    [0x08] = {'7', '&'},
+    [0x09] = {'8', '*'},
+    [0x0A] = {'9', '('},
+    [0x0B] = {'0', ')'},
+    [0x0C] = {'-', '_'},
+    [0x0D] = {'=', '+'},
+    [0x0E] = {'\b', '\b'}, // 退格键 backspace
+    [0x0F] = {'\t', '\t'},
+    // 对于字母, 需要判断是否已经按下 shift 键
+    [0x10] = {'q', 'Q'},
+    [0x11] = {'w', 'W'},
+    [0x12] = {'e', 'E'},
+    [0x13] = {'r', 'R'},
+    [0x14] = {'t', 'T'},
+    [0x15] = {'y', 'Y'},
+    [0x16] = {'u', 'U'},
+    [0x17] = {'i', 'I'},
+    [0x18] = {'o', 'O'},
+    [0x19] = {'p', 'P'},
+    [0x1A] = {'[', '{'},
+    [0x1B] = {']', '}'},
+    [0x1C] = {'\n', '\n'},
+    [0x1E] = {'a', 'A'},
+    [0x1F] = {'s', 'B'},
+    [0x20] = {'d', 'D'},
+    [0x21] = {'f', 'F'},
+    [0x22] = {'g', 'G'},
+    [0x23] = {'h', 'H'},
+    [0x24] = {'j', 'J'},
+    [0x25] = {'k', 'K'},
+    [0x26] = {'l', 'L'},
+    [0x27] = {';', ':'},
+    [0x28] = {'\'', '"'},
+    [0x29] = {'`', '~'},
+    [0x2B] = {'\\', '|'},
+    [0x2C] = {'z', 'Z'},
+    [0x2D] = {'x', 'X'},
+    [0x2E] = {'c', 'C'},
+    [0x2F] = {'v', 'V'},
+    [0x30] = {'b', 'B'},
+    [0x31] = {'n', 'N'},
+    [0x32] = {'m', 'M'},
+    [0x33] = {',', '<'},
+    [0x34] = {'.', '>'},
+    [0x35] = {'/', '?'},
+    [0x39] = {' ', ' '},
+};
+
 void kbd_init(void) {
     irq_install(IRQ1_KEYBOARD, (irq_handler_t)exception_handler_kbd);
     irq_enable(IRQ1_KEYBOARD);
+}
+
+static inline int is_make_code(uint8_t key_code) {
+    return !(key_code & 0x80); // 如果高位为 1, 则是释放键; 否则是按下键
+}
+
+static inline char get_key(uint8_t key_code) {
+    return key_code & 0x7F; // 取低 7 位
+}
+
+static void do_normal_key(uint8_t raw_code) {
+    char key = get_key(raw_code);
+    int is_make = is_make_code(raw_code);
+    switch (key) {
+    default:
+        if (is_make) {
+            key = map_table[key].normal; // 普通按键
+            log_printf("key: %d\n", key);
+        }
+        break;
+    }
 }
 
 
@@ -17,7 +94,7 @@ void do_handler_kbd(exception_frame_t *frame) { // 通过汇编实现
     }
 
     uint8_t raw_code = inb(KBD_PORT_DATA); // 读取键盘数据端口
-    log_printf("key code: %x\n", raw_code); // 打印键盘扫描码
+    // log_printf("key code: %x\n", raw_code); // 打印键盘扫描码
 
     pic_send_eoi(IRQ1_KEYBOARD); // 发送结束信号
 }
--- m_os_x86_learning/os/start/source/kernel/include/dev/kbd.h
+++ m_os_x86_learning/os/start/source/kernel/include/dev/kbd.h
@@ -2,12 +2,18 @@
 #ifndef KDB_H
 #define KDB_H
 
+#include "comm/types.h"
+
 #define KBD_PORT_DATA 0x60  // 键盘数据端口
 #define KBD_PORT_STAT  0x64  // 键盘命令端口 (有两个功能: 读和写), 读: 读键盘控制器的状态; 写: 写命令到键盘控制器(往0x64端口写命令), 这里是读命令
 #define KBD_PORT_CMD   0x64  // 键盘命令端口 (有两个功能: 读和写), 读: 读键盘控制器的状态; 写: 写命令到键盘控制器(往0x64端口写命令), 这里是写命令
 
 #define KBD_STAT_RECV_READY (1 << 0) // 键盘接收就绪标志位 (键盘控制器的状态寄存器的第 0 位)
 
+typedef struct _key_map_t {
+    uint8_t normal; // 普通按键 (eg: 对于字母取 未按下 shift 时的 ASCII 码)
+    uint8_t func;   // 第二功能 (eg: 对于字母取 按下 shift 时的 ASCII 码)
+} key_map_t;
 
 void kbd_init(void); // 初始化键盘
 void exception_handler_kbd(void); // 键盘中断处理函数
