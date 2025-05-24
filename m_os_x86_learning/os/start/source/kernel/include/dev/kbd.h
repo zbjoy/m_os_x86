eaab3b121dd2e1bdc8d6d20628c6656eae977751
zbjoy <664266432@qq.com>
2025-05-24 21:31:50 +0800

    add 宏定义

--- m_os_x86_learning/os/start/source/kernel/include/dev/kbd.h
+++ m_os_x86_learning/os/start/source/kernel/include/dev/kbd.h
@@ -14,6 +14,28 @@
 #define KEY_LSHIFT 0x2A // 左 shift 键
 #define KEY_CAPS 0x3A // caps lock 键
 
+#define KEY_CTRL 0x1D // ctrl 键, 左 CTRL 为一个字符, 右 CRTL 为两个字符, 并且第二个字符和 左CRTL 键的 ASCII 码相同
+#define KEY_RSHIFT 0x36
+#define KEY_LSHIFT 0x2A
+#define KEY_ALT 0x38 // alt 键
+
+#define KEY_F1 0x3B // F1 键
+#define KEY_F2 0x3C // F2 键
+#define KEY_F3 0x3D // F3 键
+#define KEY_F4 0x3E // F4 键
+#define KEY_F5 0x3F // F5 键
+#define KEY_F6 0x40 // F6 键
+#define KEY_F7 0x41 // F7 键
+#define KEY_F8 0x42 // F8 键
+#define KEY_F9 0x43 // F9 键
+#define KEY_F10 0x44 // F10 键
+#define KEY_F11 0x57 // F11 键
+#define KEY_F12 0x58 // F12 键
+
+#define KEY_E0 0xE0 // 扩展键 (用于组合键) (两个字符)
+#define KEY_E1 0xE1 // 扩展键 (用于组合键) (三个字符)
+
+
 typedef struct _key_map_t {
     uint8_t normal; // 普通按键 (eg: 对于字母取 未按下 shift 时的 ASCII 码)
     uint8_t func;   // 第二功能 (eg: 对于字母取 按下 shift 时的 ASCII 码)
